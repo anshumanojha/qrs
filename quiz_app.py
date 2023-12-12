ad38d594ffcf62a4b3af6112424e4bcc00503804
anshumanojha <55602333+anshumanojha@users.noreply.github.com>
2023-12-12 16:06:11 +0530

    Update quiz_app.py

--- quiz_app.py
+++ quiz_app.py
@@ -6,38 +6,35 @@ import sys
 
 print(sys.executable)
 
-def main():
-    st.title("Data Analyst Resume")
-
-    # Personal Information
-    st.header("Personal Information")
-    st.write("Name: John Doe")
-    st.write("Email: john.doe@example.com")
-    st.write("Phone: +1234567890")
-
-    # LinkedIn QR Code
-    linkedin_url = "https://www.linkedin.com/in/johndoe/"
-    st.write("LinkedIn: [LinkedIn Profile](https://www.linkedin.com/in/johndoe/)")
-    
-    # Generate QR Code for LinkedIn with a smaller size
+def generate_qr_code(linkedin_url):
     qr = qrcode.QRCode(
         version=1,
         error_correction=qrcode.constants.ERROR_CORRECT_L,
-        box_size=1,  # Adjust the box_size to make the QR code smaller
+        box_size=1,
         border=0,
     )
     qr.add_data(linkedin_url)
     qr.make(fit=True)
-
-    # Convert PilImage to bytes
+    
     img_byte_array = io.BytesIO()
     qr.make_image(fill_color="black", back_color="white").save(img_byte_array)
-    img_byte_array = img_byte_array.getvalue()
+    return img_byte_array.getvalue()
 
-    # Display the QR code using st.image
-    st.image(Image.open(io.BytesIO(img_byte_array)), caption="Scan QR Code to visit LinkedIn profile", use_column_width=True)
+def main():
+    st.title("LinkedIn QR Code Generator")
 
-    # Skills, Tools, Projects, Education, Work Experience (unchanged)
+    # User Input: LinkedIn URL
+    linkedin_url = st.text_input("Enter your LinkedIn URL:")
+    
+    if linkedin_url:
+        # Display the entered LinkedIn URL
+        st.write(f"LinkedIn URL: {linkedin_url}")
+
+        # Generate and display the QR code
+        qr_code_image = generate_qr_code(linkedin_url)
+        st.image(Image.open(io.BytesIO(qr_code_image)), caption="Scan QR Code to visit LinkedIn profile", width=100)
+
+    # Additional content (Personal Information, Skills, etc.) can be added here
 
 if __name__ == "__main__":
     main()
